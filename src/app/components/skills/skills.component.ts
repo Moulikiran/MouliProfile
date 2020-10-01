a36196f43cfe7bd686c0faa61019e72b98aa2f5d
Mouli Somesetty <msomeset@HYD-SomesetM-M.local>
2020-10-01 16:07:31 +0530

    Updated ng-circle-progress attributes.

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -13,62 +13,85 @@ export class SkillsComponent implements OnInit {
     {
       skillName:"DS & Algo",
       progress: "40",
-      color: "#ff8763"
-    },
+      color: "#ff8763",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; } },
     {
       skillName:"CoreJava",
       progress: "70",
-      color: "#9ad880"
+      color: "#9ad880",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Spring Boot",
       progress: "50",
-      color: "#4882c2"
+      color: "#4882c2",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Rest Services",
       progress: "70",
-      color: "#ff8763"
+      color: "#ff8763",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Hibernate",
       progress: "50",
-      color: "#9ad880"
+      color: "#9ad880",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Angular Js",
       progress: "70",
-      color: "#4882c2"
+      color: "#4882c2",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Angular 2+",
       progress: "60",
-      color: "#ff8763"
+      color: "#ff8763",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Tomcat",
       progress: "50",
-      color: "#9ad880"
+      color: "#9ad880",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Jenkins",
       progress: "70",
-      color: "#4882c2"
+      color: "#4882c2",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Kubernetes & Docker",
       progress: "40",
-      color: "#ff8763"
+      color: "#ff8763",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"Agile Methodlogies",
       progress: "80",
-      color: "#9ad880"
+      color: "#9ad880",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     },
     {
       skillName:"SQL Server",
       progress: "50",
-      color: "#4882c2"
+      color: "#4882c2",
+      subtitle: (progress: number): string => {
+        return `${progress}%`; }
     }
   ]
 
